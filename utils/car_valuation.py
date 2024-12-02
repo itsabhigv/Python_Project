b9379ddab3eff05bb1c477fabf1482695b860907
itsabhigv <abhijithguduru@gmail.com>
2024-12-02 22:27:30 +0000

    Made changes to car_valuation file and removed the requiremens.txt file

--- utils/car_valuation.py
+++ utils/car_valuation.py
@@ -24,7 +24,7 @@ class CarValuation:
             model = WebDriverWait(self.driver,30).until(EC.presence_of_element_located((By.XPATH,"//div[@class='d-table-cell heading'][normalize-space()='Model:']/following-sibling::div"))).get_attribute("innerText")
             year = WebDriverWait(self.driver,30).until(EC.presence_of_element_located((By.XPATH,"//div[@class='d-table-cell heading'][normalize-space()='Year:']/following-sibling::div"))).get_attribute("innerText")
             return {
-                "VEHICLE_REG" :reg_number,
+                "VARIANT_REG" :reg_number,
                 "MAKE":manufacturer,
                 "MODEL":model,
                 "YEAR": year
@@ -35,8 +35,8 @@ class CarValuation:
 
 
 class values:
-    def __init__(self, VEHICLE_REG,MAKE,MODEL,YEAR):
-        self.VEHICLE_REG = VEHICLE_REG
+    def __init__(self, VARIANT_REG,MAKE,MODEL,YEAR):
+        self.VARIANT_REG = VARIANT_REG
         self.MAKE = MAKE
         self.MODEL = MODEL
         self.YEAR = YEAR
